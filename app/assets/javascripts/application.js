efd46c3ad6d02d58ad1c7ee9d076d70eadf63b9c
levibollinger <leroy579@yahoo.com>
2019-02-21 07:53:44 +0000

    Cut/pasted contents of application.js to a new file of config.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,36 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-//user pool id us-east-2_XF6kGbMOv
-//client id 7baqk0j6u9iqthi0eucad848t6
-
-
-import * as AWS from 'aws-sdk/global';
-import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
-    
-    //This should register the user with the application
-    var poolData = {
-        UserPoolId : 'us-east-2_XF6kGbMOv', // Your user pool id here
-        ClientId : '7baqk0j6u9iqthi0eucad848t6' // Your client id here
-    };
-    var userPool = new CognitoUserPool(poolData);
-
-    var attributeList = [];
-
-    var dataEmail = {
-        Name : 'email',
-        Value : 'email@mydomain.com'
-    };
-
-    var attributeEmail = new CognitoUserAttribute(dataEmail);
-
-    attributeList.push(attributeEmail);
-
-    userPool.signUp('username', 'password', attributeList, null, function(err, result){
-        if (err) {
-            alert(err.message || JSON.stringify(err));
-            return;
-        }
-        var cognitoUser = result.user;
-        console.log('user name is ' + cognitoUser.getUsername());
-    });
