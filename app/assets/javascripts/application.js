098238502b78a3a3f25d99c78f289f12e5ac20d3
levibollinger <leroy579@yahoo.com>
2019-02-21 05:10:14 +0000

    Implemented registration using AWS User pool

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//user pool id us-east-2_XF6kGbMOv
+//client id 7baqk0j6u9iqthi0eucad848t6
+
+
+import * as AWS from 'aws-sdk/global';
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+    
+    var poolData = {
+        UserPoolId : 'us-east-2_XF6kGbMOv', // Your user pool id here
+        ClientId : '7baqk0j6u9iqthi0eucad848t6' // Your client id here
+    };
+    var userPool = new CognitoUserPool(poolData);
+
+    var attributeList = [];
+
+    var dataEmail = {
+        Name : 'email',
+        Value : 'email@mydomain.com'
+    };
+
+    var dataPhoneNumber = {
+        Name : 'phone_number',
+        Value : '+15555555555'
+    };
+    var attributeEmail = new CognitoUserAttribute(dataEmail);
+    var attributePhoneNumber = new CognitoUserAttribute(dataPhoneNumber);
+
+    attributeList.push(attributeEmail);
+    attributeList.push(attributePhoneNumber);
+
+    userPool.signUp('username', 'password', attributeList, null, function(err, result){
+        if (err) {
+            alert(err.message || JSON.stringify(err));
+            return;
+        }
+        var cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+    });
