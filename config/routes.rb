97df570a24d32b9af7ebfa564235435f0dad420e
levibollinger <leroy579@yahoo.com>
2019-02-28 21:11:14 +0000

    Should fix sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   
-  get 'registration/register'
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
