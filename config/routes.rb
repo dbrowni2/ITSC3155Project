ea6d2803413731908caeb5b0dea27618fb6463d5
Daniel Browning <dbrowni2@uncc.edu>
2019-03-27 21:06:26 +0000

    Added About us and Contact views and routes as well as partial for site nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   get 'welcome/about', to: "welcome#about"
+  get 'welcome/contact', to: "welcome#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
