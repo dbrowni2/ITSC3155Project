8f017f6c7d13b8824b81c4deb04cd882cbc5203d
abubba <47124672+abubba@users.noreply.github.com>
2019-04-02 17:08:00 -0400

    added routes for new pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/about', to: "welcome#about"
   get 'welcome/contact', to: "welcome#contact"
+  get 'welcome/submit', to: "welcome#submit"
+  get 'welcome/rewards', to: "welcome#rewards"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
