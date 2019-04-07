cca28efe0b7b30cf9170460a4f67274006fabb6f
Daniel Browning <dbrowni2@uncc.edu>
2019-04-07 01:23:39 +0000

    Added full Google oauth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
+  get 'sessions/create'
+  get 'sessions/destroy'
+ 
+
   get 'welcome/about', to: "welcome#about"
   get 'welcome/contact', to: "welcome#contact"
-  resources :submissions 
   
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
+  resources :sessions, only: [:create, :destroy]
+   get 'welcome/index'
+  resources :submissions 
+    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
