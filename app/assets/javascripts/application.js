1ee2e69c35b71b58a595e7a19c54960d1aa4f497
Daniel Browning <dbrowni2@uncc.edu>
2019-04-16 01:20:43 +0000

    Finally fixed css. I hate CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//$(window).load()
 $(window).scroll(function() {
   parallax();
 })
