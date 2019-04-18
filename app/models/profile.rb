9af16ab4ef99eb2d539d286a16135775e22dfb2c
Daniel Browning <dbrowni2@uncc.edu>
2019-04-18 16:06:17 +0000

    Added functions for tests to pass. All good

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,9 +3,23 @@ class Profile < ApplicationRecord
   
   before_save :assign_balance
   
+  
+  #This is the user's balance. This needs to detect where data was added to the table and how much of it was added in order to apply more points appropriately. 
+  #Points will be as follows: 
+  #Survey completion: +5
+  #Locations visited: +10
+  #Purchases made: +15
+  
   protected
   def assign balance
     balance = self.balance || 0
+    
+    balance += 5 if submissions.category = ('survey')
+    balance += 10 if submissions.category = ('location')
+    balance += 15 if submissions.category = ('purchase')
+    
+    #Example of a purchase being made:
+    #balance -= 100 if transaction.item = ('xboxlive')
     
     self.balance = balance
   end 
